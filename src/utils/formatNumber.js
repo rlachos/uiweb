738903cb29de7447328da7bf88c7460728482e9d
Rossio Lachos <rossio.lachos28@gmail.com>
2023-09-22 10:12:21 -0400

    eliminar comentarios

--- src/utils/formatNumber.js
+++ src/utils/formatNumber.js
@@ -1,8 +1,8 @@
 export function formatNumber(contactNumber) {
-    // Eliminar cualquier carácter que no sea un dígito
+   
     const cleanedNumber = contactNumber.replace(/\D/g, '');
   
-    // Asegurarse de que haya exactamente 10 dígitos
+  
     if (cleanedNumber.length === 10) {
       return (
         cleanedNumber.slice(0, 3) +
@@ -12,7 +12,7 @@ export function formatNumber(contactNumber) {
         cleanedNumber.slice(6)
       );
     } else if (cleanedNumber.length < 10) {
-      // Agregar ceros a la izquierda para completar 10 dígitos
+      
       const zerosToAdd = 10 - cleanedNumber.length;
       const paddedNumber = '0'.repeat(zerosToAdd) + cleanedNumber;
       return (
@@ -23,7 +23,7 @@ export function formatNumber(contactNumber) {
         paddedNumber.slice(6)
       );
     } else {
-      // Si hay más de 10 dígitos, devolver un mensaje de error o manejarlo según tus necesidades
+      
       return "Número de teléfono inválido";
     }
   }
